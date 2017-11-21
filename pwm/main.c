c6e1fd2766a48cb345ce75e889069227a1f46809
nryaskin <nik.ryaskin@gmail.com>
2017-11-21 23:51:33 +0300

    [Task4] PWM working with Pin 8(Red Led)

--- pwm/main.c
+++ pwm/main.c
@@ -13,10 +13,13 @@
 #define B_PIN_0 GPIO_Pin_0
 #define B_PIN_1 GPIO_Pin_1
 
-#define PWM_FREQ 60 
+
+#define MAX_FREQ 168000000
+#define PWM_FREQ 60
 #define PRESCALER 1680
-#define PULSE 1 
-//(PERIOD * 0.25)
+#define PERIOD (MAX_FREQ)/((PRESCALER) * (PWM_FREQ))
+#define PULSE (PERIOD)*(0.05)
+
 //168000000/1680 = 100000
 #define L_PINS (GPIO_Pin_8) | (GPIO_Pin_9) | (GPIO_Pin_10)
 
@@ -33,7 +36,7 @@ void TM_PWM_Init(void){
     TIM_OCStruct.TIM_OCPolarity = TIM_OCPolarity_Low;
     TIM_OCStruct.TIM_OutputState = TIM_OutputState_Enable;
 
-    TIM_OCStruct.TIM_Pulse = 5000;//PULSE - 1; 
+    TIM_OCStruct.TIM_Pulse = PULSE - 1; 
     TIM_OC1Init(TIM1, &TIM_OCStruct);
     TIM_OC1PreloadConfig(TIM1, TIM_OCPreload_Enable);
     TIM_CtrlPWMOutputs(TIM1, ENABLE);
@@ -46,8 +49,8 @@ void TM_PWM_Init(void){
 void Init_Timer(void){
     TIM_TimeBaseInitTypeDef tim_struct;
     RCC_APB2PeriphClockCmd(RCC_APB2Periph_TIM1, ENABLE);
-    tim_struct.TIM_Period = 9999;//(uint16_t)(PERIOD - 1);
-    tim_struct.TIM_Prescaler = 1680;//(uint16_t)(PRESCALER - 1);
+    tim_struct.TIM_Period = (uint16_t)(PERIOD - 1);
+    tim_struct.TIM_Prescaler = (uint16_t)(PRESCALER - 1);
     tim_struct.TIM_ClockDivision = 0;
     tim_struct.TIM_RepetitionCounter = 0;
     tim_struct.TIM_CounterMode = TIM_CounterMode_Up;
@@ -64,10 +67,10 @@ int main(void)
 
   /* Enable peripheral clock for GPIOA port */
   RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
-  GPIO_PinAFConfig(GPIOA, GPIO_PinSource10, GPIO_AF_TIM1);
+  GPIO_PinAFConfig(GPIOA, GPIO_PinSource8, GPIO_AF_TIM1);
 
   /* external leds*/
-  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10;
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_8;
   GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
   GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
   GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
@@ -75,14 +78,14 @@ int main(void)
   GPIO_Init(GPIOA, &GPIO_InitStructure);
 
 
+  Init_Timer();
+  TM_PWM_Init();
 
   //GPIO_SetBits(GPIOA, L_PINS);
    
   //GPIO_ResetBits(GPIOA, active_LED);
 
   
-  TM_PWM_Init();
-  Init_Timer();
   //Init_Timer_Inetr();
   while (1)
   {
