d0d286c4c2481ed29badd94a3026e6875617a9a2
nryaskin <nik.ryaskin@gmail.com>
2017-12-06 18:34:14 +0300

    [Task 5] Added ability to change intensity on button closses#2

--- pwm_lib/main.c
+++ pwm_lib/main.c
@@ -25,9 +25,10 @@
 
 
 #define COUNT 3
+#define INTESITI_LEVELS 10
 
 int turn_off;
-int j, up;
+int j, levels;
 uint16_t active_LED = GPIO_PinSource10;
 
 void TM_PWM_Init(void){
@@ -92,20 +93,89 @@ void Init_Leds(void){
 
 }
 
+void Init_Buttons(){
+    GPIO_InitTypeDef GPIO_InitStruct;
+    EXTI_InitTypeDef EXTI_InitStruct;
+    NVIC_InitTypeDef NVIC_InitStruct;
+
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_SYSCFG, ENABLE);
+
+    RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOE, ENABLE);
+    GPIO_InitStruct.GPIO_Pin = B_PIN_0 | B_PIN_1;
+    GPIO_InitStruct.GPIO_Mode = GPIO_Mode_IN;
+    GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
+    GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_UP;
+    GPIO_InitStruct.GPIO_Speed = GPIO_Speed_100MHz;
+    GPIO_Init(GPIOE, &GPIO_InitStruct);
+
+
+    SYSCFG_EXTILineConfig(EXTI_PortSourceGPIOE, EXTI_PinSource0);
+
+    EXTI_InitStruct.EXTI_Line = EXTI_Line0;
+    EXTI_InitStruct.EXTI_LineCmd = ENABLE;
+    EXTI_InitStruct.EXTI_Mode = EXTI_Mode_Interrupt;
+    EXTI_InitStruct.EXTI_Trigger = EXTI_Trigger_Rising;
+    EXTI_Init(&EXTI_InitStruct);
+
+    SYSCFG_EXTILineConfig(EXTI_PortSourceGPIOE, EXTI_PinSource1);
+
+    EXTI_InitStruct.EXTI_Line = EXTI_Line1;
+    EXTI_Init(&EXTI_InitStruct);
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_SYSCFG, DISABLE);
+
+    NVIC_InitStruct.NVIC_IRQChannel = EXTI0_IRQn;
+    NVIC_InitStruct.NVIC_IRQChannelPreemptionPriority = 0x00;
+    NVIC_InitStruct.NVIC_IRQChannelSubPriority = 0x00;
+    NVIC_InitStruct.NVIC_IRQChannelCmd = ENABLE;
+    NVIC_Init(&NVIC_InitStruct);
+
+    NVIC_InitStruct.NVIC_IRQChannelSubPriority = 0x01;
+    NVIC_InitStruct.NVIC_IRQChannel = EXTI1_IRQn;
+    NVIC_Init(&NVIC_InitStruct);
+
+}
+
+void change_intensity(int order){
+
+    levels = (levels + order + INTESITI_LEVELS) % INTESITI_LEVELS;
+
+    TIM_SetCompare1(TIM1, PERIOD * ((float)levels * 0.1));
+}
+
+
+
+void EXTI0_IRQHandler(void){
+    if(EXTI_GetITStatus(EXTI_Line0) != RESET) {
+        change_intensity(1);
+      EXTI_ClearITPendingBit(EXTI_Line0);
+    }
+}
+
+void EXTI1_IRQHandler(void){
+     if(EXTI_GetITStatus(EXTI_Line1) != RESET) {
+
+      EXTI_ClearITPendingBit(EXTI_Line1);
+    }
+}
+
 int main(void)
 {
+    j = 0;
+    levels = 0;
 
-  Init_Timer();
-  TM_PWM_Init();
-  //Init_Led_Red();
-  Init_Leds();
 
-  //GPIO_SetBits(GPIOA, L_PINS);
-  //GPIO_ResetBits(GPIOA, active_LED);
+    Init_Timer();
+    TM_PWM_Init();
+    //Init_Led_Red();
+    Init_Leds();
+    Init_Buttons();
 
-  //Init_Timer_Inetr();
-  while (1)
-  {
+    //GPIO_SetBits(GPIOA, L_PINS);
+    //GPIO_ResetBits(GPIOA, active_LED);
 
-  }
+    //Init_Timer_Inetr();
+    while (1)
+    {
+
+    }
 }
