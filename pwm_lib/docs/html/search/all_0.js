4c3d34e1f4db6ce9a548c666af44c6b5566fc29e
nryaskin <nik.ryaskin@gmail.com>
2017-12-28 21:40:57 +0300

    Documentation

---
+++ pwm_lib/docs/html/search/all_0.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['change_5fcolor',['change_color',['../pwm__lib_8h.html#a3a4700d76690200fabb3b552e9ee90e2',1,'pwm_lib.h']]],
+  ['change_5fintensity',['change_intensity',['../pwm__lib_8h.html#a8aadceaaf03fecfe8585935e7b2d4bb6',1,'pwm_lib.h']]]
+];
